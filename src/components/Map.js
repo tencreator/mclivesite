8dad14bf79c748d80c827ee9a92ba57d2db29e02
Katie Kinsey <45293208+BuyLabs@users.noreply.github.com>
2022-09-10 13:49:37 +0100

    Fixed the map fully -- I hope

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ export default function Map() {
   return (
     <div className='Container-2'>
     {/* <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> */}
-      <iframe className='Map' src='//map.mclive.ml:8180' title='Map' width={1400} height={800} allowFullScreen={false} />
+      <iframe className='Map' src='https://map.mclive.ml' title='Map' width={1400} height={800} allowFullScreen={false} />
     </div>
   )
 }

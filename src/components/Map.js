f5c874c45df26333fdb403ad74efb5aa7167e53c
Katie Kinsey <45293208+BuyLabs@users.noreply.github.com>
2022-09-10 12:32:51 +0100

    Updated Map URL

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Map() {
   return (
     <div className='Container-2'>
-      <iframe className='Map' src='https://map.mclive.ml' title='Map' width={1400} height={800} allowFullScreen={false} />
+      <iframe className='Map' src='http://144.217.49.130:8180' title='Map' width={1400} height={800} allowFullScreen={false} />
     </div>
   )
 }

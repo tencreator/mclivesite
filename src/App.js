7600059a14e01ba9f205aef2285938d5df9d26db
TenCreator <45293208+tencreator@users.noreply.github.com>
2022-09-28 20:50:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div className="Container">
         <div className="warning">
           <strong>WARNING</strong> : The MC Live Forge server is currently under maintenance temporarily
-          <button onClick='document.getElementByClass('warning').style.display = none'>X</button>
+          <button onClick="document.getElementByClass('warning').style.display = none">X</button>
          </div>
         <div className="sidebar">
           <ul>

4392ad13be57c498f1b03a440baeee2dcd14a9c3
TenCreator <45293208+tencreator@users.noreply.github.com>
2022-10-19 18:49:40 +0100

    Update Downloads.js

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -4,7 +4,7 @@ export default function Downloads() {
   return (
     <div className='Container-2'>
       <span className='downloads'>
-        click <a href='https://mega.nz/folder/j9NQDZbB#X5xz6FU4INjOxc211bGhoQ'>here</a> to download the mod pack <br />
+        click <a href='https://mega.nz/folder/P7ZBDB6I#UFK69PB0tsu_CYkSKV50Pg'>here</a> to download the mod pack <br />
         MORE COMING SOON!
       </span>
     </div>

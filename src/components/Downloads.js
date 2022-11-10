b473b2a83c4ed8e748b0629afb21ddabcc364f65
TenCreator <45293208+tencreator@users.noreply.github.com>
2022-11-10 07:52:36 +0000

    Update Downloads.js

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -4,6 +4,7 @@ export default function Downloads() {
   return (
     <div className='Container-2'>
       <span className='downloads'>
+        WARNING THE MOD PACK DOWNLOAD HAS BEEN DISABLED UNTIL WE CAN GET PERMISSION FROM THE MOD CREATORS TO USE THEIR MODS! <br />
         click here to download the mod pack <br />
         MORE COMING SOON!
       </span>

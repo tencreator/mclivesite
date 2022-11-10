a2a0de625c14ccf334edb5fbced90014f6295034
TenCreator <45293208+tencreator@users.noreply.github.com>
2022-11-10 07:47:24 +0000

    Update Downloads.js

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -4,7 +4,9 @@ export default function Downloads() {
   return (
     <div className='Container-2'>
       <span className='downloads'>
-        click <button onClick={() => {window.open('https://www.dropbox.com/s/wn2uljmltcfydel/MC%20Live%204.1.0.zip?dl=1', '_blank')}}>here</button> to download the mod pack <br />
+        click 
+          // <button onClick={() => {window.open('https://www.dropbox.com/s/wn2uljmltcfydel/MC%20Live%204.1.0.zip?dl=1', '_blank')}}>here</button> 
+        here to download the mod pack <br />
         MORE COMING SOON!
       </span>
     </div>

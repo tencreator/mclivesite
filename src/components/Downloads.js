339b6625b44330b9d5c8be7594d31d43748ab1e8
TenCreator <45293208+tencreator@users.noreply.github.com>
2023-01-17 08:21:55 +0000

    Update Downloads.js

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -4,12 +4,8 @@ export default function Downloads() {
   return (
     <div className='Container-2'>
       <span className='downloads'>
-        WARNING THE MOD PACK DOWNLOAD HAS BEEN DISABLED UNTIL WE CAN GET PERMISSION FROM THE MOD CREATORS TO USE THEIR MODS! <br />
-        click here to download the mod pack <br />
-        MORE COMING SOON!
+       THIS HAS BEEN PERMANENTLY EXPUNGED FROM OUR WEBSITE UNTIL LATER DATE!
       </span>
     </div>
   )
 }
-
-// <button onClick={() => {window.open('https://www.dropbox.com/s/wn2uljmltcfydel/MC%20Live%204.1.0.zip?dl=1', '_blank')}}>here</button> 

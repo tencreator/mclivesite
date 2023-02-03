85f3e7402850b3d77230326f1f292bd894a3478b
TenCreator <45293208+tencreator@users.noreply.github.com>
2023-02-03 11:23:11 +0000

    Update Downloads.js

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -5,6 +5,7 @@ export default function Downloads() {
     <div className='Container-2'>
       <span className='downloads'>
        THIS HAS BEEN PERMANENTLY EXPUNGED FROM OUR WEBSITE UNTIL LATER DATE!
+       <br/> UPDATE: We are currently looking for suitable hosting for our needs and then will be back up speedily!
       </span>
     </div>
   )

6b4fbd22379d593dfac5da250c96276a25458906
TenCreator <45293208+tencreator@users.noreply.github.com>
2023-02-16 15:19:58 +0000

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Map() {
   return (
     <div className='Container-2'>
-      <iframe className='Map' src='https://map.mclive.ml' title='Map' width={800} height={600} allowFullScreen={false} />
+      <iframe className='Map' src='https://map.mclive.xyz' title='Map' width={800} height={600} allowFullScreen={false} />
     </div>
   )
 }
